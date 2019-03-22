9651517665559efd80bafcf8f5f542997f5f3d55
DavidPavlovic <davidPavlovicDev@gmail.com>
2019-03-22 19:26:41 +0100

    Added project files

---
+++ js/main.js
@@ -0,0 +1,85 @@
+const dogs = [
+    {
+        name: 'Snickers',
+        age: 2
+    },
+    {
+        name: 'hugo',
+        age: 8
+    }
+];
+
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
+
+// Regular
+console.log('Hello');
+
+// Interpolated
+console.log('Hello I am a %s string', '💩');
+
+// Styled
+console.log('%c I am some great text', 'font-size: 30px; background:red; text-shadow: 10px 10px 0 blue;');
+
+// warning!
+console.warn('OH NOO');
+
+// Error :|
+console.error('Shit!');
+
+// Info
+console.info('Crocodiles eat 3-4 people per year');
+
+// Testing
+//Assert will always return false, if statment is true, nothing will happens
+const p = document.querySelector('p');
+
+console.assert(p.classList.contains('ouch'), 'Thats wrong');
+
+// clearing
+console.clear();
+
+// Viewing DOM Elements
+// dir will allow us to see class list, funcitons and bunch of other stuff that lives on that element
+console.log(p);
+console.dir(p);
+
+console.clear();
+
+// Grouping together
+// We can use group to view items in groups
+dogs.forEach(dog => {
+    // group collapse will start collapsed if group is a little bit messy
+    console.group(`${dog.name}`);
+    //console.groupCollapsed(`${dog.name}`);
+    console.log(`This is ${dog.name}`);
+    console.log(`${dog.name} is ${dog.age} years old`);
+    console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+    console.groupEnd(`${dog.name}`);
+})
+
+// counting
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+
+// timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+    .then(data => data.json)
+    .then(data => {
+        console.timeEnd('fetching data');
+        console.log(data);
+    });
